b735c12e6c8a5c3175c740c93732e78ad9f350ab
Евгений Агаджанов <geniy@live.ru>
2026-03-18 11:04:56 +0300

    Add_little_feature

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -9,10 +9,8 @@ class RecipeFilter(filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+    is_favorited = filters.BooleanFilter()
+    is_in_shopping_cart = filters.BooleanFilter()
 
     class Meta:
         model = Recipe
